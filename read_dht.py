812d30488f1802e749e47016ea483779793f0d60
Marcus Brito <mvobrito@gmail.com>
2019-10-22 16:39:11 -0300

    dht11 with firebase

--- read_dht.py
+++ read_dht.py
@@ -3,6 +3,11 @@ import Adafruit_DHT
 from time import sleep
 import datetime
 
+from firebase import firebase
+import urllib2, urllib, httplib
+import json
+import os
+
 #Configuration of GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
@@ -14,11 +19,19 @@ sensor = Adafruit_DHT.DHT11
 #Pin 17 on raspberry pi A+
 pin = 17
 
+#URL of the firebase realtime database
+url = 'https://rasp-weather.firebaseio.com/'
+#Creating firebase applicaiton
+firebase = firebase.FirebaseApplication(url, None)
+
 while True:
-    humidity, temperature = Adafruit_DHT.read(sensor, pin)
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    date_time = datetime.datetime.now()
+    data = {'Temperature': temperature, 'Humidity' : humidity, 'Date - Time' : date_time}
     if temperature != None and humidity != None:
         print('Temperature: {}'.format(temperature))
         print('Humidity: {}\n'.format(humidity))
+        firebase.post('/dht11', data)
     else:
         print('Reading error!')
     sleep(5)
