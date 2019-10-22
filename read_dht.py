2c0ccd52c89a070e5d9d3e0aaec7e7df8eb04e08
Marcus Brito <mvobrito@gmail.com>
2019-10-22 16:13:53 -0300

    dht11 v1

---
+++ read_dht.py
@@ -0,0 +1,20 @@
+import Rpi.GPIO as GPIO
+import Adafruit_DHT
+from time import sleep
+import datetime
+
+#Configuration of GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+GPIO.setwarnings(False)
+
+#Selecting sensor from Adafruit libreary
+sensor = Adafruit_DHT.DHT11
+
+#Pin 17 on raspberry pi A+
+pin = 17
+
+while True:
+    humidity, temperature = Adafruit_DHT.read(sensor, pin)
+    print('Temperature: {}\n'.format(temperature))
+    print('Temperature: {}\n'.format(humidity))
